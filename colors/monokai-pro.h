a019f1dc3f37a608a6d7c5ebcb7b2993877550c6
hp27596 <hp27596@gmail.com>
2022-07-14 18:06:52 +0700

    initial commit

---
+++ colors/monokai-pro.h
@@ -0,0 +1,10 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#FCFCFA", "#2D2A2E" },
+	[SchemeSel] = { "#202020", "#4fb9af" },
+	[SchemeSelHighlight] = { "#9cff00", "#202020" },
+	[SchemeNormHighlight] = { "#9cff00", "#202020" },
+	[SchemeOut] = { "#2D2A2E", "#78DCE8" },
+	[SchemeMid] = { "#FCFCFA", "#202020" },
+};
+
